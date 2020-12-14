fb71be5a2174f841febaeec2016263a68bbbaf9f
Happy-11 <1056972599@qq.com>
2020-12-15 01:03:04 +0800

    修改了常量配置

--- Assets/Scripts/Common/Constants.cs
+++ Assets/Scripts/Common/Constants.cs
@@ -1,13 +1,16 @@
 ﻿/****************************************************
     文件：Constants.cs
 	作者：Happy-11
-    日期：#CreateTime#
-	功能：Nothing
+    日期：2020年12月15日01:00:25
+	功能：常量配置
 *****************************************************/
 
 using UnityEngine;
 
 public class Constants : MonoBehaviour 
 {
+    public void test()
+    {
 
+    }
 }
